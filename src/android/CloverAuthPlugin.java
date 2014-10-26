49935ff38655aa90d020c28ce2aa3b57e386d408
srikanthnadella <srikanth.nadella@gmail.com>
2014-10-26 13:22:30 -0400

    Update CloverAuthPlugin.java

--- src/android/CloverAuthPlugin.java
+++ src/android/CloverAuthPlugin.java
@@ -1,4 +1,4 @@
-package com.termtegrity.spotskim.cloverplugin;
+package com.sripra.termtegrity.cloverplugin;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -7,22 +7,27 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.pm.PackageManager;
-
+import android.app.Activity;
+import android.content.Intent;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import android.view.MenuItem;
+import android.view.View;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.TextView;
 import com.clover.sdk.util.CloverAccount;
-import com.clover.sdk.util.CloverAuth;
 import com.clover.sdk.v1.ResultStatus;
 import com.clover.sdk.v1.ServiceConnector;
 import com.clover.sdk.v1.merchant.Merchant;
@@ -30,25 +35,6 @@ import com.clover.sdk.v1.merchant.MerchantAddress;
 import com.clover.sdk.v1.merchant.MerchantConnector;
 import com.clover.sdk.v1.merchant.MerchantIntent;
 import com.clover.sdk.v1.app.AppNotificationIntent;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpException;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.HttpVersion;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.params.HttpClientParams;
-import org.apache.http.conn.params.ConnManagerParams;
-import org.apache.http.conn.params.ConnPerRouteBean;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -56,23 +42,34 @@ import android.app.PendingIntent;
 import android.support.v4.app.NotificationCompat.Builder;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.app.NotificationCompat;
+//import com.termtegrity.spotskim.cloverplugin.R;
+//import com.termtegrity.spotskim.cloverplugin.SpotskimNotification;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 import java.util.Date;
 
-public class CloverAuthPlugin extends CordovaPlugin{
-    public static final String ACTION_GET_MERCHANT = "getMerchant1";
+public class CloverAuthPlugin extends CordovaPlugin implements MerchantConnector.OnMerchantChangedListener, ServiceConnector.OnServiceConnectedListener{
+    public static final String ACTION_GET_MERCHANT = "getMerchant";
     private static final String TAG = "CloverPlugin";
-      private CallbackContext notificationCallbackContext = null;
+    private CallbackContext notificationCallbackContext = null;
     BroadcastReceiver receiver;
-    //private static final int REQUEST_ACCOUNT = 0;
-    //private MerchantConnector merchantConnector;
-    private Account account;
-    private String merchantID = "TEST";
-    private String deviceID = "TEST1";
     
-     private void sendNotification(Intent notificationIntent){
+    @Override
+    public void onMerchantChanged(Merchant merchant) {
+        updateMerchant("merchant changed", null, merchant);
+    }
+    
+    @Override
+    public void onServiceConnected(ServiceConnector connector) {
+        Log.i(TAG, "service connected: " + connector);
+    }
+    
+    @Override
+    public void onServiceDisconnected(ServiceConnector connector) {
+        Log.i(TAG, "service disconnected: " + connector);
+    }
+    
+    private void sendNotification(Intent notificationIntent){
         if (this.notificationCallbackContext != null) {
             
             //this.notificationCallbackContext.success("Notification:" + notificationIntent.getStringExtra(AppNotificationIntent.EXTRA_APP_EVENT) +
@@ -90,6 +87,61 @@ public class CloverAuthPlugin extends CordovaPlugin{
             this.notificationCallbackContext.sendPluginResult(result);
         }
         
+    }
+    
+    
+    //    public void onResume() {
+    //        super.onResume(true);
+    //
+    //        cordova.getActivity().registerReceiver(this.receiver,
+    //                                                                 new IntentFilter(AppNotificationIntent.ACTION_APP_NOTIFICATION));
+    //    }
+    //
+    //
+    //    public void onPause() {
+    //        cordova.getActivity().unregisterReceiver(this.receiver);
+    //        super.onPause(true);
+    //    }
+    
+    
+    /**
+     * Creates a JSONObject with the current battery information
+     *
+     * @param batteryIntent the current battery information
+     * @return a JSONObject containing the battery status information
+     */
+    private JSONObject getBatteryInfo(Intent batteryIntent) {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("level", batteryIntent.getIntExtra(android.os.BatteryManager.EXTRA_LEVEL, 0));
+            obj.put("isPlugged", batteryIntent.getIntExtra(android.os.BatteryManager.EXTRA_PLUGGED, -1) > 0 ? true : false);
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+        return obj;
+    }
+    
+    /**
+     * Updates the JavaScript side whenever the battery changes
+     *
+     * @param batteryIntent the current battery information
+     * @return
+     */
+    private void updateBatteryInfo(Intent batteryIntent) {
+        sendUpdate(this.getBatteryInfo(batteryIntent), true);
+    }
+    
+    /**
+     * Create a new plugin result and send it back to JavaScript
+     *
+     * @param connection the network info to set as navigator.connection
+     */
+    private void sendUpdate(JSONObject info, boolean keepCallback) {
+        if (this.notificationCallbackContext != null) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, info);
+            result.setKeepCallback(keepCallback);
+            this.notificationCallbackContext.sendPluginResult(result);
+        }
     }
     
     @SuppressWarnings("deprecation")
@@ -129,16 +181,13 @@ public class CloverAuthPlugin extends CordovaPlugin{
         mNotificationManager.notify(1, mBuilder.build());
     }
     
- 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    	callbackContext.error("Invalid action");
-    	return false;
-    	/*
         try {
             if (ACTION_GET_MERCHANT.equals(action)) {
-            
-            if (this.notificationCallbackContext == null) {
+                //JSONObject arg_object = args.getJSONObject(0);
+                
+                if (this.notificationCallbackContext == null) {
                     this.notificationCallbackContext = callbackContext;
                 }
                 
@@ -157,35 +206,30 @@ public class CloverAuthPlugin extends CordovaPlugin{
                     cordova.getActivity().registerReceiver(this.receiver, intentFilter);
                 }
                 
+                startAccountChooser();
+                connect();
+                getMerchant();
+                //------------------------------------
+                JSONObject obj = new JSONObject();
+                try {
+                    obj.put("MerchantID", merchantID);
+                    obj.put("DeviceID", deviceID);
+                } catch (JSONException e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
                 
-               // account = CloverAccount.getAccount(this.cordova.getActivity().getApplicationContext());
-                if (account == null) {
-            callbackContext.error("Account not found");
-            return false;
-        }
-        //getMerchant();
-        //queryWebService();
-        JSONObject obj = new JSONObject();
-        //if(merchantID != null && deviceID != null){
-		
-        try {
-            obj.put("MerchantID", merchantID);
-            obj.put("DeviceID", deviceID);
-        } catch (JSONException e) {
-            Log.e(TAG, e.getMessage(), e);
-        }
-        //}
-        //else{
-        //callbackContext.error("Merchant not found");
-        //    return false;
-        //}
-        
-	PluginResult result = new PluginResult(PluginResult.Status.OK, obj);
-        result.setKeepCallback(true);
-        callbackContext.sendPluginResult(result);
-
-        return true;
-        }
+                PluginResult result = new PluginResult(PluginResult.Status.OK, obj);
+                result.setKeepCallback(true);
+                callbackContext.sendPluginResult(result);
+                //-------------------------------
+                
+                // if(merchantConnector == null)
+                //                     callbackContext.error("MerchantConnector is null");
+                //                 else
+                //                     callbackContext.success(merchantID+ "/" +deviceID);
+                //                 disconnect();
+                return true;
+            }
             callbackContext.error("Invalid action");
             return false;
         } catch(Exception e) {
@@ -193,140 +237,87 @@ public class CloverAuthPlugin extends CordovaPlugin{
             callbackContext.error("Exception: " + e.getMessage());
             return false;
         }
-        */
+    }
+    
+    private static final int REQUEST_ACCOUNT = 0;
+    private MerchantConnector merchantConnector;
+    private Account account;
+    private String merchantID;
+    private String deviceID;
+    
+    private void startAccountChooser() {
+        //        Intent intent = AccountManager.newChooseAccountIntent(null, null, new String[]{CloverAccount.CLOVER_ACCOUNT_TYPE}, false, null, null, null, null);
+        //        startActivityForResult(intent, REQUEST_ACCOUNT);
+        AccountManager accountManager = AccountManager.get(this.cordova.getActivity().getApplicationContext());
+        Account[] accounts = accountManager.getAccountsByType(CloverAccount.CLOVER_ACCOUNT_TYPE);
+        for (Account acc : accounts){
+            account = acc;
+            break;
+        }
+        
     }
     
     
-//     private void getMerchant() {
-//         Merchant merchant = null;
-//         try {
-//             merchant = merchantConnector.getMerchant();
-//         }
-//         catch (Exception e){
-//             
-//         }
-//         updateMerchant();
-//         
-//     }
-//     
-//     private void updateMerchant() {
-//         merchantID = result.getId();
-//         deviceID = result.getDeviceId();
-//     }
-
-    private void log(String text) {
-        Log.i(TAG, text);
+    private void connect() {
+        disconnect();
+        if (account != null) {
+            merchantConnector = new MerchantConnector(cordova.getActivity(), account, this);
+            merchantConnector.setOnMerchantChangedListener(this);
+            merchantConnector.connect();
+        }
     }
     
-    private void queryWebService() {
-        new AsyncTask<Void, String, Void>() {
-
-            @Override
-            protected void onProgressUpdate(String... values) {
-                String logString = values[0];
-                log(logString);
-            }
-
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    publishProgress("Requesting auth token");
-                    CloverAuth.AuthResult authResult = CloverAuth.authenticate(cordova.getActivity(), account);
-                    publishProgress("Successfully authenticated as " + account + ".  authToken=" + authResult.authToken + ", authData=" + authResult.authData);
-
-                    if (authResult.authToken != null && authResult.baseUrl != null) {
-                        CustomHttpClient httpClient = CustomHttpClient.getHttpClient();
-                        String getNameUri = "/v2/merchant/name";
-                        String url = authResult.baseUrl + getNameUri + "?access_token=" + authResult.authToken;
-                        publishProgress("requesting merchant id using: " + url);
-                        String result = httpClient.get(url);
-                        JSONTokener jsonTokener = new JSONTokener(result);
-                        JSONObject root = (JSONObject) jsonTokener.nextValue();
-                        merchantID = root.getString("merchantId");
-                        deviceID = root.getString("deviceId");
-                        publishProgress("received merchant id: " + merchantID);
-
-                    }
-                } catch (Exception e) {
-                    publishProgress("Error retrieving merchant info from server" + e);
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                // mButton.setEnabled(true);
-            }
-        }.execute();
-    }
-
-    static class CustomHttpClient extends DefaultHttpClient {
-        private static final int CONNECT_TIMEOUT = 60000;
-        private static final int READ_TIMEOUT = 60000;
-        private static final int MAX_TOTAL_CONNECTIONS = 5;
-        private static final int MAX_CONNECTIONS_PER_ROUTE = 3;
-        private static final int SOCKET_BUFFER_SIZE = 8192;
-        private static final boolean FOLLOW_REDIRECT = false;
-        private static final boolean STALE_CHECKING_ENABLED = true;
-        private static final String CHARSET = HTTP.UTF_8;
-        private static final HttpVersion HTTP_VERSION = HttpVersion.HTTP_1_1;
-        private static final String USER_AGENT = "CustomHttpClient"; // + version
-
-        public static CustomHttpClient getHttpClient() {
-            CustomHttpClient httpClient = new CustomHttpClient();
-            final HttpParams params = httpClient.getParams();
-            HttpProtocolParams.setUserAgent(params, USER_AGENT);
-            HttpProtocolParams.setContentCharset(params, CHARSET);
-            HttpProtocolParams.setVersion(params, HTTP_VERSION);
-            HttpClientParams.setRedirecting(params, FOLLOW_REDIRECT);
-            HttpConnectionParams.setConnectionTimeout(params, CONNECT_TIMEOUT);
-            HttpConnectionParams.setSoTimeout(params, READ_TIMEOUT);
-            HttpConnectionParams.setSocketBufferSize(params, SOCKET_BUFFER_SIZE);
-            HttpConnectionParams.setStaleCheckingEnabled(params, STALE_CHECKING_ENABLED);
-            ConnManagerParams.setTimeout(params, CONNECT_TIMEOUT);
-            ConnManagerParams.setMaxTotalConnections(params, MAX_TOTAL_CONNECTIONS);
-            ConnManagerParams.setMaxConnectionsPerRoute(params, new ConnPerRouteBean(MAX_CONNECTIONS_PER_ROUTE));
-
-            return httpClient;
-        }
-
-        public String get(String url) throws IOException, HttpException {
-            String result;
-            HttpGet request = new HttpGet(url);
-            HttpResponse response = execute(request);
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode == HttpStatus.SC_OK) {
-                HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    result = EntityUtils.toString(entity);
-                } else {
-                    throw new HttpException("Received empty body from HTTP response");
-                }
-            } else {
-                throw new HttpException("Received non-OK status from server: " + response.getStatusLine());
-            }
-            return result;
-        }
-
-        @SuppressWarnings("unused")
-        public String post(String url, String body) throws IOException, HttpException {
-            String result;
-            HttpPost request = new HttpPost(url);
-            HttpEntity bodyEntity = new StringEntity(body);
-            request.setEntity(bodyEntity);
-            HttpResponse response = execute(request);
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode == HttpStatus.SC_OK) {
-                HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    result = EntityUtils.toString(entity);
-                } else {
-                    throw new HttpException("Received empty body from HTTP response");
-                }
-            } else {
-                throw new HttpException("Received non-OK status from server: " + response.getStatusLine());
-            }
-            return result;
+    private void disconnect() {
+        if (merchantConnector != null) {
+            merchantConnector.disconnect();
+            merchantConnector = null;
         }
     }
+    
+    
+    private void getMerchant() {
+        Merchant merchant = null;
+        try {
+            merchant = merchantConnector.getMerchant();
+        }
+        catch (Exception e){
+            
+        }
+        merchantID = merchant.getId();
+        deviceID = merchant.getDeviceId();
+        
+    }
+    
+    private void updateMerchant(String status, ResultStatus resultStatus, Merchant result) {
+        merchantID = result.getId();
+        deviceID = result.getDeviceId();
+    }
+    
+    //    /**
+    //     * Stop notification receiver.
+    //     */
+    //    public void onDestroy() {
+    //        removeNotificationListener();
+    //    }
+    //
+    //    /**
+    //     * Stop notification receiver.
+    //     */
+    //    public void onReset() {
+    //        removeNotificationListener();
+    //    }
+    //
+    //    /**
+    //     * Stop the notification receiver and set it to null.
+    //     */
+    //    private void removeNotificationListener() {
+    //        if (this.receiver != null) {
+    //            try {
+    //                this.cordova.getActivity().unregisterReceiver(this.receiver);
+    //                this.receiver = null;
+    //            } catch (Exception e) {
+    //                Log.e(TAG, "Error unregistering notification receiver: " + e.getMessage(), e);
+    //            }
+    //        }
+    //    }
 }
